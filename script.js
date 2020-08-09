9ce6fb52bc1a045674651b8f2c9c177b0e5a7c15
Valerie <valerie.flores64@gmail.com>
2020-08-08 17:29:25 -0700

    updated webpack

--- script.js
+++
@@ -1,3 +0,0 @@
-// As a user, I want the application to load quickly
-// As a user, I want to use the Food Festival application even if I don't have an internet connection
-// As a user, I want to download the app to the home screen on my mobile device
